33689e10e2d974cb4225a35a1d1918a764d1c5f9
raHasan22 <r.ahmad.hasan.r@gmail.com>
2025-05-26 10:35:03 +0600

    theme added

---
+++ assets/global-script.js
@@ -0,0 +1,96 @@
+// Hero slider
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // Responsive breakpoints
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2.2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4.2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 7.2,
+      spaceBetween: 20,
+    },
+  },
+});
+// review slider
+var swiper1 = new Swiper(".testi-slider", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Instructor slider
+
+var swiper2 = new Swiper(".single-testi-slider", {
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination-ins",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 0px (mobile)
+    0: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px (tablet/laptop)
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1024px (desktop)
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+// student review
+var swiper3 = new Swiper(".StudentReviewSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
